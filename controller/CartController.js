322977d4bf7b21af6d36d3c4e59d471a10d7cc60
Navaneetha1603 <navaneethakandlapelli@gmail.com>
2020-12-06 13:09:14 +0530

    commit

--- controller/CartController.js
+++ controller/CartController.js
@@ -13,11 +13,11 @@ const createCartItems=async(req,res)=>{
     let id=req.params.productId;
     console.log(id);
     try{
-             // const cart=new Cart(req.body);
-             const cart=new Cart();
-             cart.user_email=req.body.user_email;
-             cart.product_id=req.query.id;
-             cart.product_qty=req.body.product_qty;
+             const cart=new Cart(req.body);
+            //  const cart=new Cart();
+            //  cart.user_email=req.body.user_email;
+            //  cart.product_id=id;
+            //  cart.product_qty=req.body.product_qty;
              try{
                  await cart.save();
                  return res.json({data:cart});
@@ -60,39 +60,37 @@ const createCartItems=async(req,res)=>{
 //        return res.status(200).json(err);
 //    }
 // }
-const productById=async(req,res)=>{
-    let id=req.params.Id;
-    try{
-         const products=await product.find({"product_id":id});
-         return res.json({data:products});
-    }
-    catch(err){
-        return res.status(200).json(err);
-    }
-}
-const createProducts=async(req,res)=>{
-    const Product =new product(req.body);
-    try{
-        await Product.save();
-        return res.json({data:Product});
-    }
-    catch(err){
-        return res.status(200).json(err);
-    }
-}
-const getProducts=async(req,res)=>{
-    try{
-        const products=await product.find({});
-        return res.json({data:products});
-    }
-    catch(err){
-        return res.status(200).json(err);
-    }
-}
+// const productById=async(req,res)=>{
+//     let id=req.params.Id;
+//     try{
+//          const products=await product.find({"product_id":id});
+//          return res.json({data:products});
+//     }
+//     catch(err){
+//         return res.status(200).json(err);
+//     }
+// }
+// const createProducts=async(req,res)=>{
+//     const Product =new product(req.body);
+//     try{
+//         await Product.save();
+//         return res.json({data:Product});
+//     }
+//     catch(err){
+//         return res.status(200).json(err);
+//     }
+// }
+// const getProducts=async(req,res)=>{
+//     try{
+//         const products=await product.find({});
+//         return res.json({data:products});
+//     }
+//     catch(err){
+//         return res.status(200).json(err);
+//     }
+// }
 module.exports={
     getCartItems,
-    createProducts,
-    createCartItems,
-    productById,
-    getProducts
+    createCartItems
+    
 }
