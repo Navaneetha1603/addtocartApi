7dfdf269d3ea2dad4159759724858a48eb69470d
Navaneetha1603 <navaneethakandlapelli@gmail.com>
2020-12-06 11:51:56 +0530

    commit

--- controller/CartController.js
+++ controller/CartController.js
@@ -10,34 +10,56 @@ const getCartItems=async(req,res)=>{
     }
 }
 const createCartItems=async(req,res)=>{
-   let id=req.params.productId;
-   console.log(id);
-   try{
-        const Product=await product.find({"product_id":id});
-        console.log(Product);
-        if(!Product){
-            return res.status(200).json(`${_id} is not found`);
-        }
-        else{
-            // const cart=new Cart(req.body);
-            const cart=new Cart();
-            cart.user_email=req.body.user_email;
-            cart.product_id=req.query.id;
-            cart.product_qty=req.body.product_qty;
-            try{
-                await cart.save();
-                return res.json({data:cart});
-            }
-            catch(err){
-                return res.status(200).json(err);
-            }
-            // return res.json({data:Product})
-        }
-   }
-   catch(err){
-       return res.status(200).json(err);
-   }
-}
+    let id=req.params.productId;
+    console.log(id);
+    try{
+             // const cart=new Cart(req.body);
+             const cart=new Cart();
+             cart.user_email=req.body.user_email;
+             cart.product_id=req.query.id;
+             cart.product_qty=req.body.product_qty;
+             try{
+                 await cart.save();
+                 return res.json({data:cart});
+             }
+             catch(err){
+                 return res.status(200).json(err);
+             }
+         
+    }
+    catch(err){
+        return res.status(200).json(err);
+    }
+ }
+// const createCartItems=async(req,res)=>{
+//    let id=req.params.productId;
+//    console.log(id);
+//    try{
+//         const Product=await product.find({"product_id":id});
+//         console.log(Product);
+//         if(!Product){
+//             return res.status(200).json(`${_id} is not found`);
+//         }
+//         else{
+//             // const cart=new Cart(req.body);
+//             const cart=new Cart();
+//             cart.user_email=req.body.user_email;
+//             cart.product_id=req.query.id;
+//             cart.product_qty=req.body.product_qty;
+//             try{
+//                 await cart.save();
+//                 return res.json({data:cart});
+//             }
+//             catch(err){
+//                 return res.status(200).json(err);
+//             }
+//             // return res.json({data:Product})
+//         }
+//    }
+//    catch(err){
+//        return res.status(200).json(err);
+//    }
+// }
 const productById=async(req,res)=>{
     let id=req.params.Id;
     try{
