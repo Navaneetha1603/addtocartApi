817396da2880ec52551599bd6168f5527411a267
Navaneetha1603 <navaneethakandlapelli@gmail.com>
2020-12-06 13:09:57 +0530

    commit

--- router/CartRouter.js
+++ router/CartRouter.js
@@ -5,10 +5,10 @@ const controller=require("../controller/CartController");
 //     res.send({message:"hello world"})
 // })
 router.get('/cartItems',controller.getCartItems);
-router.post('/createProducts',controller.createProducts);
+// router.post('/createProducts',controller.createProducts);
 router.post('/add/:productId',controller.createCartItems);
-router.get('/add/:Id',controller.createCartItems);
-router.get('/getProducts/:Id',controller.productById);
-router.get('/getProducts',controller.getProducts);
+// router.get('/add/:Id',controller.createCartItems);
+// router.get('/getProducts/:Id',controller.productById);
+// router.get('/getProducts',controller.getProducts);
 
 module.exports=router;
